fc024f78a4e5a1971a81ac2d5c80f61014ccc236
gabriel121souza <gabriel121souza@gmail.com>
2020-10-27 21:21:49 -0300

    about View

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,6 +12,8 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,13 +21,17 @@ public class Main extends Application {
 			ScrollPane scrollPane =loader.load();
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
-			Scene mainScene = new Scene(scrollPane);
+			
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("SAMple javaFX application");
 			primaryStage.show();
 		}catch (IOException e){
 			e.printStackTrace();
 		}
+	}
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 	public static void main(String[] args) {
 		launch(args);
